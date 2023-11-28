8d543db4526af6184a4dd4dc9dec41dd9919b4a8
Jennifer Otis <jenni.otis7@gmail.com>
2023-11-28 18:04:23 -0500

    aiden

---
+++ Assets/Scripts/Aiden.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiden : MonoBehaviour
+{
+    public string Charactername = "Aiden";
+    public int aidenNumber = 9;
+    public string enemyName = "evil";
+        public int spaceMoney = 100;
+    public bool hasMoney = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (hasMoney)
+        {
+            
+        }
+
+        if (3 > 1)
+        {
+
+
+        }
+        if (1<100019)
+        {
+
+
+
+        }
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
